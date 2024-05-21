21103402b644c6c9551203bfe899453c812fafa3
Guilherme Betsa <ybtyza@gmail.com>
2024-05-20 22:49:48 -0300

    feat: 🎸 (ex05) Programa principal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 const calcularSalarioLiquido = require("./calculo_salario_liquido");
-let salarioBruto = 7000
-console.log(calcularSalarioLiquido(salarioBruto).toFixed(2))
+const calcularInss = require("./calculo_inss");
+const calcularImpostoDeRenda = require("./calculo_imposto_de_renda");
+
+const readline = require('readline');
+
+const input = readline.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+input.question("Nome do colaborador: ", (nome) => {
+        input.question("CPF do colaborador: ", (cpf) => {
+            input.question("Salário bruto: ", (salarioBruto) => {
+                const inss = calcularInss(salarioBruto);
+                const impostoDeRenda = calcularImpostoDeRenda(salarioBruto);
+                const salarioLiquido = calcularSalarioLiquido(salarioBruto);
+                console.log(`--- Folha de pagamento ---\nNome: ${nome}\nCPF: ${cpf}\nSalário bruto: ${salarioBruto}\nINSS: ${inss}\nImposto de renda: ${impostoDeRenda}\nSalário líquido: ${salarioLiquido}`);
+            })
+        })
+    })
